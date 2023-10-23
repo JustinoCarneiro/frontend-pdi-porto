4b06743457868e7868dd712676f7a20b0d60e1b7
FP Glasser <glasserpaulo95@hotmail.com>
2023-10-23 15:07:35 -0300

    chore: Configuração e estrutura inicial do projeto

---
+++ src/components/card-panel-display/CardPanelDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardPanelDisplay = () => {
+  return (
+    <div>CardPanelDisplay</div>
+  )
+}
+
+export default CardPanelDisplay
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Topbar } from './topbar/Topbar'
+export { default as Sidebar } from './sidebar/Sidebar'
+export { default as InfoPanel } from './info-panel/InfoPanel'
+export { default as CardPanelDisplay } from './card-panel-display/CardPanelDisplay'
