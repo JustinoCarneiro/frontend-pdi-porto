c03d2892966bb5ffb044d6e1de462e7e0b823287
FP Glasser <glasserpaulo95@hotmail.com>
2023-10-24 00:22:07 -0300

    style: arquivos de estilos foram adicionados as paginas

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './style.module.css'
+
+const Topbar = () => {
+  return (
+    <nav>
+      
+    </nav>
+  )
+}
+
+export default Topbar
--- src/pages/layout/RootLayout.js
+++ src/pages/layout/RootLayout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import style from './style.module.css'
 
 const RootLayout = () => {
   return (
